c61616cf855995dbd85c1b73231cae969f76a825
Arpan Adlakhiya <40575581+mrr0b0t-0x1@users.noreply.github.com>
2021-02-04 19:21:31 +0530

    Added sleep

--- scripts/reddit_module.py
+++ scripts/reddit_module.py
@@ -3,6 +3,7 @@ from globals import *
 import json
 from random import randint
 from colorama import Fore
+import time
 
 # Generate random headers
 def generate_headers():
@@ -26,15 +27,16 @@ def gather_info(username):
     result_dir = CWD / "scripts" / "results" / username / "reddit"
 
     # Target URLs and headers
+    reddit_user_info = {}
     overview_url = 'https://www.reddit.com/user/' + username + '/overview/.json'
     about_url = 'https://www.reddit.com/user/' + username + '/about/.json'
     headers = generate_headers()
 
     # Request the JSON data of user
-    reddit_user_info = {
-        'overview': requests.get(url=overview_url, headers=headers).json(),
-        'about': requests.get(url=about_url, headers=headers).json()
-    }
+    reddit_user_info['overview'] = requests.get(url=overview_url, headers=headers).json()
+    # Sleep for 5 seconds to avoid getting banned
+    time.sleep(5)
+    reddit_user_info['about'] = requests.get(url=about_url, headers=headers).json()
 
     # Store result data to file
     try:
@@ -43,7 +45,8 @@ def gather_info(username):
 
         # TODO: Remove this in final build
         # Print result data
-        print('\nReddit Data:')
-        print(json.dumps(reddit_user_info, indent=2))
+        # print('\nReddit Data:')
+        # print(json.dumps(reddit_user_info, indent=2))
+        print('\nReddit data fetched\n')
     except Exception as err:
         print(Fore.RED + type(err).__name__ + Fore.RESET + ": " + str(err))
