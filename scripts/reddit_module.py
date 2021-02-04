1742c4278b647de45474465872ffd171b4367084
Arpan Adlakhiya <40575581+mrr0b0t-0x1@users.noreply.github.com>
2021-02-04 16:55:32 +0530

    Updated headers in reddit_module

--- scripts/reddit_module.py
+++ scripts/reddit_module.py
@@ -4,16 +4,16 @@ import json
 from random import randint
 
 # Generate random headers
-def return_headers():
+def generate_headers():
     headers = {
-        'Host': 'www.facebook.com',
+        'Host': 'www.reddit.com',
         'User-Agent': USER_AGENTS[randint(0, len(USER_AGENTS) - 1)],
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
         'Referer': REFERERS[randint(0, len(REFERERS) - 1)],
         'Connection': 'keep-alive',
         'DNT': '1',
-        "Upgrade-Insecure-Requests": "1",
+        "Upgrade-Insecure-Requests": "1"
     }
     return headers
 
@@ -31,16 +31,18 @@ def gather_info(username):
     # Target URLs and headers
     overview_url = 'https://www.reddit.com/user/' + username + '/overview/.json'
     about_url = 'https://www.reddit.com/user/' + username + '/about/.json'
-    headers = return_headers()
+    headers = generate_headers()
 
     # Request the JSON data of user
-    reddit_user_info = {'overview': requests.get(url=overview_url, headers=headers).json(),
-                        'about': requests.get(url=about_url, headers=headers).json()}
+    reddit_user_info = {
+        'overview': requests.get(url=overview_url, headers=headers).json(),
+        'about': requests.get(url=about_url, headers=headers).json()
+    }
 
     # Store result data to file
     try:
-        # with open(result_dir / (username + ".json"), "w+") as handle:
-        #     json.dump(reddit_user_info, handle, indent=2)
+        with open(result_dir / (username + ".json"), "w+") as handle:
+            json.dump(reddit_user_info, handle, indent=2)
 
         # TODO: Remove this in final build
         # Print result data
