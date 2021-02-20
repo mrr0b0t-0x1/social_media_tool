e8b807b5fa5e95fd10fb9b5800b03c0536656368
Arpan Adlakhiya <40575581+mrr0b0t-0x1@users.noreply.github.com>
2021-02-20 22:10:22 +0530

    Connected to Python backend

--- ui/index.js
+++ ui/index.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const {spawn} = require('child_process');
 
+const ROOT_DIR = path.resolve(__dirname, '..')
+
 
 btnSearch = document.getElementById('btnSearch')
 btngetdata = document.getElementById('btngetdata')
@@ -25,7 +27,7 @@ btnSearch.addEventListener('click', function(){
     // }); 
     //console.log("Name:"+ userName.value);
 
-    const childPython = spawn('python', ['test.py',userName.value]);
+    const childPython = spawn('python', ['../scripts/main.py',userName.value]);
     childPython.stdout.on('data', (data) => {
         console.log(`Python Script Output: ${data}`);
     })
@@ -35,38 +37,69 @@ btnSearch.addEventListener('click', function(){
 //Handling Click event for Getdata Btn  --------------------------------------------------------
 
 btngetdata.addEventListener('click', function(){
-    var mysql = require('mysql');
-
-    var connection = mysql.createConnection({
-        host:"localhost",
-        user:"root",
-        password:null,
-        database:"electron_test"
-    });
-
-    connection.connect((err) => {
-        if(err){
-            return console.log(err.stack);
-        }
-
-        console.log("Connection Established successfully");
-    });
+    // var mysql = require('mysql');
+    //
+    // var connection = mysql.createConnection({
+    //     host:"localhost",
+    //     user:"root",
+    //     password:null,
+    //     database:"electron_test"
+    // });
+    //
+    // connection.connect((err) => {
+    //     if(err){
+    //         return console.log(err.stack);
+    //     }
+    //
+    //     console.log("Connection Established successfully");
+    // });
 
 
     //Execute the Query
 
-    $queryString = 'SELECT * FROM `contacts` WHERE `id` = "2";';
+    // $queryString = 'SELECT * FROM `contacts` WHERE `id` = "2";';
+    //
+    // connection.query($queryString, (err, rows, fields) => {
+    //     if(err)
+    //     {
+    //         return console.log("Some error occured",err);
+    //     }
+    //     console.log(rows);
+    // })
+    //
+    //
+    // connection.end(() => {
+    //     console.log("Connection Successfully Closed");
+    // });
+    let {PythonShell} = require('python-shell')
 
-    connection.query($queryString, (err, rows, fields) => {
-        if(err)
-        {
-            return console.log("Some error occured",err);
-        }
-        console.log(rows);
-    })
+    let options = {
+        mode: 'text',
+        pythonPath: '../venv1/bin/python',
+        pythonOptions: ['-u'], // get print results in real-time
+        scriptPath: '../scripts',
+        args: [userName.value]
+    };
 
-
-    connection.end(() => {
-        console.log("Connection Successfully Closed");
+    PythonShell.run('main.py', options, function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        console.log('results: ', results);
     });
+
+    // sends a message to the Python script via stdin
+    // pyshell.send(userName.value);
+
+    // pyshell.on('message', function (message) {
+    //   // received a message sent from the Python script (a simple "print" statement)
+    //   console.log(message);
+    // });
+
+    // end the input stream and allow the process to exit
+    // pyshell.end(function (err,code,signal) {
+    //   if (err) throw err;
+    //   console.log('The exit code was: ' + code);
+    //   console.log('The exit signal was: ' + signal);
+    //   console.log('finished');
+    // });
 });
