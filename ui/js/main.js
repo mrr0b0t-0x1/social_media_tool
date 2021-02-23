3aeb2f5f82bea5d180ad12d8cb2e215f3dd0fbef
Arpan Adlakhiya <40575581+mrr0b0t-0x1@users.noreply.github.com>
2021-02-23 15:23:02 +0530

    Changed browser window size

--- ui/js/main.js
+++ ui/js/main.js
@@ -10,7 +10,7 @@ let win;
 
 function createWindow () {
 
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 1024, height: 768})
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, '../index.html'),
