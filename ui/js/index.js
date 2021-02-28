11e00fb466d3cbd9af4cb86fa3d1be60878a12c3
Arpan Adlakhiya <40575581+mrr0b0t-0x1@users.noreply.github.com>
2021-02-28 19:17:32 +0530

    Updated UI, display stored user data

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,38 +1,147 @@
+const fs = require('fs');
+
 $(document).ready(function () {
-    const userName = document.getElementById('userName')
-    const usernameForm = document.getElementById('usernameForm')
-    const liveResults = document.getElementById('liveResults')
-    const btnSearch = document.getElementById('btnSearch')
-    const btnSearchAgain = document.getElementById('btnSearchAgain')
-    const btnCancel = document.getElementById('btnCancel')
-    const resultTabs = document.getElementById('resultTabs')
+    const userName = document.getElementById('userName');
+    const usernameForm = document.getElementById('usernameForm');
+    const liveResults = document.getElementById('liveResults');
+    const btnSearchUser = document.getElementById('btnSearchUser');
+    const btnNewSearch = document.getElementById('btnNewSearch');
+    const btnCancelSearch = document.getElementById('btnCancelSearch');
+    const resultTabs = document.getElementById('resultTabs');
+    const resultsTabContent = document.getElementById('results-tabContent');
+    const btnUpdateData = document.getElementById('btnUpdateUserData');
+    const btnRemoveData = document.getElementById('btnRemoveUserFromDB');
+    const alertsView = document.getElementById('alertsView');
+
+    const siteList = [
+        'facebook',
+        'instagram',
+        'twitter',
+        'reddit',
+        'linkedin'
+    ];
+    const tabAndPanes = {
+        'facebook': [
+            document.getElementById('list-facebook-list'),
+            document.getElementById('list-facebook')
+        ],
+        'instagram': [
+            document.getElementById('list-instagram-list'),
+            document.getElementById('list-instagram')
+        ],
+        'twitter': [
+            document.getElementById('list-twitter-list'),
+            document.getElementById('list-twitter')
+        ],
+        'reddit': [
+            document.getElementById('list-reddit-list'),
+            document.getElementById('list-reddit')
+        ],
+        'linkedin': [
+            document.getElementById('list-linkedin-list'),
+            document.getElementById('list-linkedin')
+        ]
+    }
 
     function toggleLiveResults() { $('#searchBox, #liveResults').toggleClass(
               'offset-1 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3').toggleClass(
                       'col-sm-6 col-md-6 col-lg-6');
     }
 
-    function traverse(obj, parent) {
-        for (let key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                if (typeof obj[key] === 'object') {
-                    let el = document.createElement("div");
-                    el.innerText = key + ": "
-                    parent.appendChild(el);
-                    traverse(obj[key], el);
-                } else {
-                    let txt = document.createElement("span");
-                    txt.innerText = key + ": " + obj[key]
-                    txt.innerHTML += "<br />"
-                    parent.appendChild(txt);
-                }
-            }
+    function showDismissableAlert(alert) {
+        if ($('#alertsView > div').text().trim() !== '') {
+            $('.alert').alert('close');
+            setTimeout(function () {
+                showDismissableAlert(alert);
+            }, 250);
+        } else {
+            $('#alertsView > div')[0].innerHTML =
+                '<div id="' + alert['id'] + '" class="alert ' + alert['class'] + ' m-0 text-center fade" role="alert">' +
+                alert['msg'] +
+                '</div>';
+
+            const el = document.getElementById(alert['id']);
+            setTimeout(function () {
+                $(el).addClass('show');
+                setTimeout(function () {
+                    $(el).alert('close');
+                }, 1995);
+            }, 5);
         }
     }
-    // $('#list-results-list').click()
 
-    $('body').on('DOMSubtreeModified', resultTabs, function () {
-        if ($(resultTabs).is(':empty')) {
+    function readJSONFile(path, callback) {
+        try {
+            const data = fs.readFileSync(path);
+            const dataJSON = JSON.parse(data.toString());
+            return callback && callback(null, dataJSON);
+        } catch (err) {
+            return callback && callback(err);
+        }
+    }
+    function traverseDataTree(obj, site) {
+        Object.keys(obj).forEach(function (key) {
+            // console.log(key);
+            if (obj[key] !== null) {
+                if (obj[key] !== null && typeof obj[key] === 'object')
+                    traverseDataTree(obj[key], site);
+                else {
+                    tabAndPanes[site][1].innerHTML += key + ": " + obj[key] + "<br />"
+                    // console.log(nestKey + " -> " + key + ": " + obj[key]);
+                    // let txt = document.createElement("span");
+                    // txt.innerText = key + ": " + obj[key]
+                    // txt.innerHTML += "<br />"
+                    // parent.appendChild(txt);
+                }
+            }
+        });
+    }
+    function traverseDBTree(obj, site) {
+        Object.keys(obj).forEach(function (key) {
+            console.log(key);
+            if (obj[key] !== null) {
+                if (obj[key] !== null && typeof obj[key] === 'object')
+                    traverseDBTree(obj[key], site);
+                else {
+                    if (obj[key].split('.').pop() === 'json') {
+                        readJSONFile(obj[key], (err, data) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log(data);
+                                traverseDataTree(data, site);
+                            }
+                        });
+                    }
+                    // tabAndPanes[site][1].innerHTML += key + ": " + obj[key] + "<br />"
+                    // console.log(nestKey + " -> " + key + ": " + obj[key]);
+                    // let txt = document.createElement("span");
+                    // txt.innerText = key + ": " + obj[key]
+                    // txt.innerHTML += "<br />"
+                    // parent.appendChild(txt);
+                }
+            }
+        });
+    }
+    function parseDBData(obj) {
+        // console.log(Object.keys(obj));
+        Object.keys(obj['sites_found']).forEach(function (site) {
+            if (siteList.includes(site)) {
+                // show those tabs whose sites are found
+                $(tabAndPanes[site][0]).removeClass('d-none');
+                $(tabAndPanes[site][1]).removeClass('d-none');
+
+                traverseDBTree(obj['sites_found'][site], site);
+            }
+        });
+    }
+
+    // $('#results-tab').children().first().css(
+    //     'border-radius', '10px 10px 0 0'
+    // )
+
+    $('body').on('DOMSubtreeModified', resultsTabContent, function () {
+        if ($('#results-tabContent > div').text().trim() === '') {
             $(resultTabs).addClass('d-none')
             $('#noResults').removeClass('d-none')
         } else {
@@ -41,7 +150,13 @@ $(document).ready(function () {
         }
     });
 
-    $(btnSearch).click(function () {
+
+    $(btnSearchUser).click(function () {
+        showDismissableAlert({
+            'id': 'search-alert',
+            'class': 'alert-primary',
+            'msg': 'Starting search...'
+        });
         // Execute backend script
         let {PythonShell} = require('python-shell')
 
@@ -61,32 +176,46 @@ $(document).ready(function () {
         pyshell.on('message', function (message) {
             // Received a message sent from the Python script (a simple "print" statement)
             if (message.ERROR) {
-                liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>&emsp;<span class='text-danger'>Error: " + message.ERROR + "</span></span>"
+                liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>" +
+                    "&emsp;<span class='text-danger'>Error: " + message.ERROR + "</span></span>"
                 console.log("Error: " + message.ERROR);
             }
             else if (message.ELAPSED_TIME) {
-                liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>&emsp;<span class='text-success'>Done! Elapsed Time: " + message.ELAPSED_TIME + "</span></span>"
+                liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>" +
+                    "&emsp;<span class='text-success'>Done! Elapsed Time: " + message.ELAPSED_TIME + "</span></span>"
                 console.log("Done! Elapsed time: " + message.ELAPSED_TIME);
             }
             else if (message.INFO) {
-                liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>&emsp;" + message.INFO + "</span>"
+                liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>" +
+                    "&emsp;" + message.INFO + "</span>"
                 console.log("Info: " + message.INFO);
             }
             else if (message.DATA) {
-                liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>&emsp;<span class='text-info'>Data Fetched!</span></span>"
-                traverse(message.DATA, resultTabs);
-                console.log("Data: ", message.DATA);
+                liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>" +
+                    "&emsp;<span class='text-info'>Data Fetched!</span></span>"
+                parseDBData(message.DATA);
+                // Show appropriate divs and add border-radius to last div
+                $('#user-resultTab > h4')[0].innerText = userName.value;
+                $('#results-tab > a:first').addClass('active');
+                $('#results-tabContent > div:first').addClass('show active');
+                $('#results-tab > a:not(.d-none):last').css('border-radius', '0 0 10px 10px');
             }
         });
 
         // end the input stream and allow the process to exit
         pyshell.end(function (err,code,signal) {
             if (err) throw err;
-            $(btnCancel).attr('disabled', true)
-            $(btnSearchAgain).attr('disabled', false)
+            $(btnCancelSearch).attr('disabled', true)
+            $(btnNewSearch).attr('disabled', false)
+
+            showDismissableAlert({
+                'id': 'done-alert',
+                'class': 'alert-success',
+                'msg': 'Done! Crunching data...'
+            });
             setTimeout(function() {
                 $('#list-results-list').click()
-            }, 500);
+            }, 1000);
             console.log('The exit code was: ' + code);
             console.log('The exit signal was: ' + signal);
             console.log('finished');
@@ -96,16 +225,16 @@ $(document).ready(function () {
         // Make changes in UI
         if ($(liveResults).hasClass('hide')) {
             setTimeout(function () {
-                $(liveResults).removeClass('hide').addClass('show');
-                $(btnSearchAgain).attr('disabled', true);
-                $(btnCancel).attr('disabled', false)
+                $(liveResults).removeClass('d-none');
+                $(btnNewSearch).attr('disabled', true);
+                $(btnCancelSearch).attr('disabled', false)
 
                 setTimeout(function () {
-                    $(liveResults).removeClass('d-none');
-                    $(btnSearch).addClass('d-none').attr('disabled', true);
-                    $('#btnSearchAgain').removeClass('d-none');
+                    $(liveResults).removeClass('hide').addClass('show');
+                    $(btnSearchUser).addClass('d-none').attr('disabled', true);
+                    $('#btnNewSearch').removeClass('d-none');
                     setTimeout(function (){
-                       $('#btnSearchAgain, #btnCancel').removeClass('hide').addClass('show');
+                       $('#btnNewSearch, #btnCancelSearch').removeClass('hide').addClass('show');
                     }, 50);
                 }, 50);
 
@@ -113,27 +242,28 @@ $(document).ready(function () {
 
             toggleLiveResults();
             $(userName).attr('disabled', true);
-            $(btnSearch).removeClass('show').addClass('hide');
+            $(btnSearchUser).removeClass('show').addClass('hide');
 
-            liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>&emsp;<span class='text-info'>Starting search...</span></span>";
+            liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>&emsp;" +
+                "<span class='text-info'>Starting search...</span></span>";
         }
 
-        $(btnCancel).click(function (){
+        $(btnCancelSearch).click(function (){
             pyshell.kill()
         });
     });
 
-    $(btnSearchAgain).click(function () {
+    $(btnNewSearch).click(function () {
         if ($(liveResults).hasClass('show')) {
             setTimeout(function() {
                 toggleLiveResults();
                 $(liveResults).addClass('d-none');
                 $(userName).attr('disabled', false);
-                $(btnSearch).removeClass('d-none');
-                $('#btnSearchAgain').addClass('d-none');
+                $(btnSearchUser).removeClass('d-none');
+                $('#btnNewSearch').addClass('d-none');
 
                 setTimeout(function () {
-                   $(btnSearch).removeClass('hide').addClass('show');
+                   $(btnSearchUser).removeClass('hide').addClass('show');
                 }, 100);
 
                 $(usernameForm)[0].reset();
@@ -142,16 +272,40 @@ $(document).ready(function () {
             }, 500);
 
             $(liveResults).removeClass('show').addClass('hide');
-            $(btnSearch).attr('disabled', false);
-            $('#btnSearchAgain, #btnCancel').removeClass('show').addClass('hide');
-            $(resultTabs).empty();
+            $(btnSearchUser).attr('disabled', false);
+            $('#btnNewSearch, #btnCancelSearch').removeClass('show').addClass('hide');
+            $('#results-tabContent > div').empty().removeClass('show active');
+            $('#results-tab > a').removeClass('active');
+            Object.keys(siteList).forEach(function (site) {
+                $(tabAndPanes[siteList[site]][0]).addClass('d-none');
+                $(tabAndPanes[siteList[site]][1]).addClass('d-none');
+            });
         }
     });
 
-    $(btnCancel).click(function (){
-        $(btnCancel).attr('disabled', true)
-        $(btnSearchAgain).attr('disabled', false)
+    $(btnCancelSearch).click(function () {
+        $(btnCancelSearch)
+            .attr('disabled', true)
+            .data('clicked', true)
+        $(btnNewSearch).attr('disabled', false)
 
-        liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>&emsp;<span class='text-danger'>Interrupted!</span> Search cancelled.</span>";
+        liveResults.firstElementChild.innerHTML += "<span class='d-block'><span class='text-grey'>❯</span>&emsp;" +
+            "<span class='text-danger'>Interrupted!</span> Search cancelled.</span>";
+    });
+
+    $(btnUpdateData).click(function () {
+        showDismissableAlert({
+            'id': 'update-alert',
+            'class': 'alert-success',
+            'msg': 'Update button clicked'
+        });
+    });
+
+    $(btnRemoveData).click(function () {
+        showDismissableAlert({
+            'id': 'remove-alert',
+            'class': 'alert-danger',
+            'msg': 'Remove button clicked'
+        });
     });
 });
